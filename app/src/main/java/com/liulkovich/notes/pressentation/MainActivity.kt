be2c04bb49126f93c47f998c7fed495d94ba7ae9
Ivan Liulkovich <ahapxuct09@gmail.com>
2025-09-04 14:20:09 +0300

    Jetpack Compose navigation

--- app/src/main/java/com/liulkovich/notes/pressentation/MainActivity.kt
+++ app/src/main/java/com/liulkovich/notes/pressentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.liulkovich.notes.pressentation.navigation.CustomNavGraph
 import com.liulkovich.notes.pressentation.navigation.NavGraph
 import com.liulkovich.notes.pressentation.ui.theme.NotesTheme
 
--- app/src/main/java/com/liulkovich/notes/pressentation/navigation/NavGraph.kt
+++ app/src/main/java/com/liulkovich/notes/pressentation/navigation/NavGraph.kt
@@ -3,49 +3,98 @@ package com.liulkovich.notes.pressentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.liulkovich.notes.pressentation.screens.creation.CreateNoteScreen
 import com.liulkovich.notes.pressentation.screens.editing.EditNoteScreen
 import com.liulkovich.notes.pressentation.screens.notes.NotesScreen
 
 @Composable
 fun NavGraph() {
-    val screen = remember {
-        mutableStateOf<Screen>(Screen.Notes)
-    }
-    when(val currentScreen = screen.value) {
-        Screen.CreateNote -> {
-            CreateNoteScreen(
-                onFinished = {
-                    screen.value = Screen.Notes
-                }
-            )
-        }
-        is Screen.EditNote -> {
-            EditNoteScreen(
-                noteId = currentScreen.noteId,
-                onFinished = {
-                    screen.value = Screen.Notes
-                }
-            )
-        }
-        Screen.Notes -> {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Notes.route
+    ) {
+        composable(Screen.Notes.route) {
             NotesScreen(
                 onNoteClick = {
-                    screen.value = Screen.EditNote(it.id)
+                    navController.navigate(Screen.EditNote.route)
                 },
                 onAddNoteClick = {
-                    screen.value = Screen.CreateNote
+                    navController.navigate(Screen.CreateNote.route)
+                }
+            )
+        }
+        composable(Screen.CreateNote.route) {
+            CreateNoteScreen(
+                onFinished = {
+                    navController.popBackStack()
+                }
+            )
+        }
+        composable(Screen.EditNote.route) {
+            EditNoteScreen(
+                noteId = 5,
+                onFinished = {
+                    navController.popBackStack()
                 }
             )
         }
     }
 }
 
-sealed interface Screen {
 
-    data object Notes: Screen
 
-    data object CreateNote: Screen
+@Composable
+fun CustomNavGraph() {
+    val screen = remember {
+        mutableStateOf<CustomScreen>(CustomScreen.Notes)
+    }
+    when(val currentScreen = screen.value) {
+        CustomScreen.CreateNote -> {
+            CreateNoteScreen(
+                onFinished = {
+                    screen.value = CustomScreen.Notes
+                }
+            )
+        }
+        is CustomScreen.EditNote -> {
+            EditNoteScreen(
+                noteId = currentScreen.noteId,
+                onFinished = {
+                    screen.value = CustomScreen.Notes
+                }
+            )
+        }
+        CustomScreen.Notes -> {
+            NotesScreen(
+                onNoteClick = {
+                    screen.value = CustomScreen.EditNote(it.id)
+                },
+                onAddNoteClick = {
+                    screen.value = CustomScreen.CreateNote
+                }
+            )
+        }
+    }
+}
 
-    data class EditNote(val noteId: Int): Screen
+sealed class Screen(val route: String) {
+
+    data object Notes: Screen("notes")
+
+    data object CreateNote: Screen("create_note")
+
+    data object EditNote: Screen("edit_note")
+}
+
+sealed interface CustomScreen {
+
+    data object Notes: CustomScreen
+
+    data object CreateNote: CustomScreen
+
+    data class EditNote(val noteId: Int): CustomScreen
 }
