08953cc32301efc680d2b6cd0fe3708433123607
Ivan Liulkovich <ahapxuct09@gmail.com>
2025-09-04 15:38:35 +0300

    Passing parameters in navigation

--- app/src/main/java/com/liulkovich/notes/pressentation/navigation/NavGraph.kt
+++ app/src/main/java/com/liulkovich/notes/pressentation/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package com.liulkovich.notes.pressentation.navigation
 
+import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,7 +21,7 @@ fun NavGraph() {
         composable(Screen.Notes.route) {
             NotesScreen(
                 onNoteClick = {
-                    navController.navigate(Screen.EditNote.route)
+                    navController.navigate(Screen.EditNote.createRoute(it.id)) //edit_note/id
                 },
                 onAddNoteClick = {
                     navController.navigate(Screen.CreateNote.route)
@@ -35,8 +36,10 @@ fun NavGraph() {
             )
         }
         composable(Screen.EditNote.route) {
+            val noteId = Screen.EditNote.getNoteId(it.arguments)
+
             EditNoteScreen(
-                noteId = 5,
+                noteId = noteId,
                 onFinished = {
                     navController.popBackStack()
                 }
@@ -87,7 +90,16 @@ sealed class Screen(val route: String) {
 
     data object CreateNote: Screen("create_note")
 
-    data object EditNote: Screen("edit_note")
+    data object EditNote: Screen("edit_note/{note_id}") {
+
+        fun createRoute(noteId: Int): String {
+            return "edit_note/$noteId"
+        }
+
+        fun getNoteId(arguments: Bundle?): Int {
+            return arguments?.getString("note_id")?.toInt() ?: 0
+        }
+    }
 }
 
 sealed interface CustomScreen {
