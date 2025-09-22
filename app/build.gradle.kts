7904b912eb07173069433915e0a767a5cc3cbe28
Ivan Liulkovich <ahapxuct09@gmail.com>
2025-09-22 16:14:17 +0300

    Create icon app, add Splash Screen Api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
